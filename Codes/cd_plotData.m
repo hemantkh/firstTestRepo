4d3593f792302197061e852e8431ccd11090331c
hemantkh <hemant50505@gmail.com>
2018-06-01 14:31:11 +0530

    plotting only sample points

--- Codes/cd_plotData.m
+++ Codes/cd_plotData.m
@@ -2,13 +2,16 @@ clear all;          close all;
 
 load ../Data/dataSeriesProcessed.mat
 
-plot(timeSeries,dataSeries);
+% plotting all values of no use; plot representative values only
+repIndex = 1:10:length(timeSeries);
+
+plot(timeSeries(:,repIndex),dataSeries(:,repIndex));
 title('Sorted (randomly generated) time series');
 xlabel('Date');     ylabel('Random relative humidity');
 legend('Location','NorthWest');
 xlim([timeSeries(1) timeSeries(end)]);
 
-print('../Plots/testFigure','-dpng','-r0');
+print('../Plots/testFigure','-dpng','-r86');
 
 pause(1);       
 close all;
