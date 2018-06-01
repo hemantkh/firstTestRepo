69f219846edac2eb8f390c834b7b13fe3416bf85
hemantkh <33367121+hemantkh@users.noreply.github.com>
2018-06-01 14:26:21 +0530

    removed sorting

--- Codes/cd_processData.m
+++ Codes/cd_processData.m
@@ -2,7 +2,8 @@ clear all; close all;
 
 load ../Data/testData.mat
 
-dataSeries = (sort(dataSeries,2,'ascend'));
+
+% dataSeries = (sort(dataSeries,2,'ascend')); % sort the data
+dataSeries = dataSeries*100; % do not sort; convert to percetage values
 
 save('../Data/dataSeriesProcessed.mat','dataSeries','timeSeries');
-
